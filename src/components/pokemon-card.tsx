125a53033cd98e9999206cd3e028507ea7c8e904
Simon <simon.dieny@gmail.com>
2019-11-25 21:37:50 +0100

    step(refactor-computed-property-in-helpers-folder)

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
+import formatDate from '../helpers/format-date';
+import formatType from '../helpers/format-type';
 import './pokemon-card.css';
 
 type Props = {
@@ -18,55 +20,6 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
   const hideBorder = () => {
     setColor('#f5f5f5');
   };
-
-  const formatDate = (date: Date): string => {
-    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-  }
-
-  const formatType = (type: string): string => {
-    let color: string;
- 
-    switch (type) {
-      case 'Feu': 
-        color = 'red lighten-1'; 
-        break; 
-      case 'Eau': 
-        color = 'blue lighten-1'; 
-        break; 
-      case 'Plante': 
-        color = 'green lighten-1'; 
-        break; 
-      case 'Insecte': 
-        color = 'brown lighten-1'; 
-        break; 
-      case 'Normal': 
-        color = 'grey lighten-3'; 
-        break; 
-      case 'Vol': 
-        color = 'blue lighten-3'; 
-        break; 
-      case 'Poison': 
-        color = 'deep-purple accent-1'; 
-        break; 
-      case 'Fée': 
-        color = 'pink lighten-4'; 
-        break; 
-      case 'Psy': 
-        color = 'deep-purple darken-2'; 
-        break; 
-      case 'Electrik': 
-        color = 'lime accent-1'; 
-        break; 
-      case 'Combat': 
-        color = 'deep-orange'; 
-        break; 
-      default: 
-        color = 'grey'; 
-        break; 
-    }
- 
-    return `chip ${color}`;
-  }
  
   return (
     <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
---
+++ src/helpers/format-date.ts
@@ -0,0 +1,5 @@
+const formatDate = (date: Date): string => {
+  return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+}
+   
+export default formatDate;
