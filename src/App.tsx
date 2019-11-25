970d689f527434fb0234833003dbb5917024b734
Simon <simon.dieny@gmail.com>
2019-11-25 12:37:32 +0100

    step(correction-pokemon-state)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React, { FunctionComponent, useState } from 'react';
- 
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
+
 const App: FunctionComponent = () => {
-  const [name, setName] = useState<String>('React');
-   
-  return (
-    <h1>Hello, {name} !</h1>
-  )
+ const [pokemons] = useState<Pokemon[]>(POKEMONS);
+
+ return (
+  <div>
+   <h1>Pokédex</h1>
+   <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+  </div>
+  );
 }
- 
+
 export default App;
