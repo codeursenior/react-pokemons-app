41fb6fe448e04ae2008bd39dabd72191e64e3bcc
Simon <simon.dieny@gmail.com>
2019-11-25 13:12:15 +0100

    step(correction-pokemon-list)

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,26 @@ const App: FunctionComponent = () => {
 
  return (
   <div>
-   <h1>Pokédex</h1>
-   <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+  <h1 className="center">Pokédex</h1>
+   <div className="container"> 
+    <div className="row"> 
+     {pokemons.map(({id, name, picture, created}) => (
+      <div className="col s6 m4" key={id}>
+       <div className="card horizontal">
+        <div className="card-image"> 
+         <img src={picture} alt={name}/>
+        </div>
+        <div className="card-stacked">
+         <div className="card-content">
+          <p>{name}</p>
+          <p><small>{created.toString()}</small></p>
+         </div>
+        </div>
+       </div> 
+      </div>
+     ))}
+    </div> 
+   </div>
   </div>
   );
 }
