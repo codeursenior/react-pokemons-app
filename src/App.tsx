a8c34dab13c06c489f9841ed658e49a8f39be427
Simon <simon.dieny@gmail.com>
2019-11-25 21:52:03 +0100

    step(setup-react-navigation)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React, { FunctionComponent } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PokemonsList from './pages/pokemon-list';
-
+import PokemonsDetail from './pages/pokemon-detail';
+ 
 const App: FunctionComponent = () => {
+ 
   return (
-  <PokemonsList />
+    <Router>
+      <div>
+      <nav> 
+        <div className="nav-wrapper teal">
+          <Link to="/" className="brand-logo center">Pokédex</Link>
+        </div> 
+      </nav>
+      <Switch>
+        <Route exact path="/" component={PokemonsList} />
+        <Route exact path="/pokemons" component={PokemonsList} />
+        <Route path="/pokemons/:id" component={PokemonsDetail} />
+      </Switch>
+      </div>
+    </Router>
   );
 }
-
+ 
 export default App;
