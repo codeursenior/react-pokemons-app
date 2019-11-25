7f1bf487c0793ca6493a858743a40eb602ec77d5
Simon <simon.dieny@gmail.com>
2019-11-25 21:20:22 +0100

    step(add-computed-property-type)

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -22,6 +22,51 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
   const formatDate = (date: Date): string => {
     return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
   }
+
+  const formatType = (type: string): string => {
+    let color: string;
+ 
+    switch (type) {
+      case 'Feu': 
+        color = 'red lighten-1'; 
+        break; 
+      case 'Eau': 
+        color = 'blue lighten-1'; 
+        break; 
+      case 'Plante': 
+        color = 'green lighten-1'; 
+        break; 
+      case 'Insecte': 
+        color = 'brown lighten-1'; 
+        break; 
+      case 'Normal': 
+        color = 'grey lighten-3'; 
+        break; 
+      case 'Vol': 
+        color = 'blue lighten-3'; 
+        break; 
+      case 'Poison': 
+        color = 'deep-purple accent-1'; 
+        break; 
+      case 'Fée': 
+        color = 'pink lighten-4'; 
+        break; 
+      case 'Psy': 
+        color = 'deep-purple darken-2'; 
+        break; 
+      case 'Electrik': 
+        color = 'lime accent-1'; 
+        break; 
+      case 'Combat': 
+        color = 'deep-orange'; 
+        break; 
+      default: 
+        color = 'grey'; 
+        break; 
+    }
+ 
+    return `chip ${color}`;
+  }
  
   return (
     <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
@@ -33,6 +78,9 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
           <div className="card-content">
             <p>{pokemon.name}</p>
             <p><small>{formatDate(pokemon.created)}</small></p>
+            {pokemon.types.map(type => (
+              <span key={type} className={formatType(type)}>{type}</span>
+            ))}
           </div>
         </div>
       </div> 
