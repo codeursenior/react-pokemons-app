3806f50cda8d458883258f962dc09d0a688deae6
Simon <simon.dieny@gmail.com>
2019-11-25 13:41:44 +0100

    step(add-event-pokemon-card-component)

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
 
@@ -8,10 +8,20 @@ type Props = {
 };
 
 const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
-  
+ 
+  const [color, setColor] = useState<string>();
+ 
+  const showBorder = () => {
+    setColor(borderColor);
+  };
+ 
+  const hideBorder = () => {
+    setColor('#f5f5f5');
+  };
+ 
   return (
-    <div className="col s6 m4">
-      <div className="card horizontal" style={{ borderColor: borderColor }}>
+    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+      <div className="card horizontal" style={{ borderColor: color }}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
         </div>
@@ -25,5 +35,5 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
     </div>
   );
 }
-
+ 
 export default PokemonCard;
