19f04043ffb9f7c4c92c9c1e9b19fffbd525caf9
Simon <simon.dieny@gmail.com>
2019-11-25 12:17:25 +0100

    chap(hello-world)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+ 
+const App: FunctionComponent = () => {
+ const name: String = 'React';
+   
+ return (
+  <h1>Hello, {name} !</h1>
+ )
+}
+ 
+export default App;
