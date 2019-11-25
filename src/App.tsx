95fd86b5eb56fd7987e4244696f3e1a4874925df
Simon <simon.dieny@gmail.com>
2019-11-25 12:39:37 +0100

    step(correction-pokemon-effect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemon';
 
 const App: FunctionComponent = () => {
- const [pokemons] = useState<Pokemon[]>(POKEMONS);
+ const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+ useEffect(() => {
+  setPokemons(POKEMONS);
+ }, []);
 
  return (
   <div>
