35757c8b104758d446cab45bbcff919836f9d8b6
Simon <simon.dieny@gmail.com>
2019-12-17 18:13:34 +0100

    step(setup-fake-backend)

--- src/helpers/format-date.ts
+++ src/helpers/format-date.ts
@@ -1,4 +1,4 @@
-const formatDate = (date: Date): string => {
+const formatDate = (date: Date = new Date()): string => {
   return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 }
    
