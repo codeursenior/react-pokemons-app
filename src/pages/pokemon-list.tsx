0feef4efd635a249b4e6c7598032c88435942081
Simon <simon.dieny@gmail.com>
2019-12-17 21:26:17 +0100

    step(get-pokemons-request)

--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -1,25 +1,29 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from '../models/pokemon';
-import POKEMONS from '../models/mock-pokemon';
 import PokemonCard from '../components/pokemon-card';
 
 const PokemonList: FunctionComponent = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
-    setPokemons(POKEMONS);
+    // GET Request
+    fetch('http://localhost:3001/pokemons')
+    .then(response => response.json())
+    .then((pokemons) => {
+      setPokemons(pokemons);
+    });
   }, []);
 
   return (
-<div>
+    <div>
       <h1 className="center">Pokédex</h1>
       <div className="container"> 
-    <div className="row"> 
-      {pokemons.map(pokemon => (
-        <PokemonCard key={pokemon.id} pokemon={pokemon}/>
-      ))}
-</div>
-</div>
+        <div className="row"> 
+          {pokemons.map(pokemon => (
+            <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+          ))}
+        </div>
+      </div>
     </div> 
   );
 }
