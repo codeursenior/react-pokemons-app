c28c13f441466ead79f576d76ba414bb1c156f62
Simon <simon.dieny@gmail.com>
2019-12-17 22:53:39 +0100

    step(handle-fetch-error)

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -4,16 +4,22 @@ export default class PokemonService {
 
   static getPokemons(): Promise<Pokemon[]> {
     return fetch('http://localhost:3001/pokemons')
-      .then(response => response.json());
+      .then(response => response.json())
+      .catch(error => this.handleError(error));
   }
 
   static getPokemon(id: number): Promise<Pokemon|null> {
     return fetch(`http://localhost:3001/pokemons/${id}`)
       .then(response => response.json())
-      .then(data => this.isEmpty(data) ? null : data);
+      .then(data => this.isEmpty(data) ? null : data)
+      .catch(error => this.handleError(error));
   }
 
   static isEmpty(data: Object): boolean {
     return Object.keys(data).length === 0;
+  }
+
+  static handleError(error: Error): void {
+    console.error(error);
   }
 }
