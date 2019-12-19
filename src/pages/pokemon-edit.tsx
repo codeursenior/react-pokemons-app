ac0fb9fe22369a0487b55d251a350cff5b1217eb
Simon <simon.dieny@gmail.com>
2019-12-19 08:44:51 +0100

    step(exercice-isEdit-pokemon-form)

--- src/pages/pokemon-edit.tsx
+++ src/pages/pokemon-edit.tsx
@@ -20,7 +20,7 @@ const PokemonEdit: FunctionComponent<RouteComponentProps<Params>> = ({ match }) 
       { pokemon ? (
         <div className="row">
             <h2 className="header center">Éditer { pokemon.name }</h2>
-            <PokemonForm pokemon={pokemon}></PokemonForm>
+            <PokemonForm pokemon={pokemon} isEditForm={true}></PokemonForm>
         </div>
       ) : (
         <h4 className="center">Aucun pokémon à afficher !</h4>
