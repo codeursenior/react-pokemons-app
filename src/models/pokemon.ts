8af811c365e9d5008a86066b568e34bfbfc231d4
Simon <simon.dieny@gmail.com>
2019-12-19 08:47:27 +0100

    fix(pokemon-model-default-value)

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -11,8 +11,8 @@ export default class Pokemon {
      id: number,
      hp: number = 100,
      cp: number = 10,
-     name: string = 'name',
-     picture: string = 'http://...',
+     name: string = '...',
+     picture: string = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/XXX.png',
      types: Array<string> = ['Normal'],
      created: Date = new Date()
     ) {
