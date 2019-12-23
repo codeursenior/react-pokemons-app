5852db55abb3dd1803dced2ab087e071d06c7f7e
Simon <simon.dieny@gmail.com>
2019-12-23 10:55:23 +0100

    step(authentication-service)

---
+++ src/services/authentication-service.ts
@@ -0,0 +1,15 @@
+export default class AuthenticationService {
+
+  static isAuthenticated:boolean = false
+
+  static login(username: string, password: string): Promise<boolean> {
+    const isAuthenticated = (username === 'pikachu' && password === 'pikachu');
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.isAuthenticated = isAuthenticated;
+        resolve(isAuthenticated);
+      }, 1000);
+    });
+  }
+}
